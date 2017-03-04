5d394a8786836638150062aa4b339a539ee6b3cc
magic_student <mutita.com@gmail.com>
2017-03-04 18:43:54 +0700

    add init file and manifest file

---
+++ addons/hw_escpos/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'ESC/POS Hardware Driver',
+    'category': 'Point of Sale',
+     'version': '10.0.1.0',
+    'sequence': 6,
+   'author':'winwindev@outlook.co.th',
+    'website': 'https://www.thaiodoo.org',
+    'summary': 'Hardware Driver for ESC/POS Printers and Cashdrawers',
+    'description': """
+ESC/POS Hardware Driver
+=======================
+
+This module allows openerp to print with ESC/POS compatible printers and
+to open ESC/POS controlled cashdrawers in the point of sale and other modules
+that would need such functionality.
+
+""",
+    'depends': ['hw_proxy'],
+    'external_dependencies': {
+        'python' : ['usb.core','serial','qrcode'],
+    },
+}
